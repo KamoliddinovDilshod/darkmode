ea4801f743010ad14ccf15f7089dc12a115d7fca
KamoliddinovDilshod <dilshodkamoliddinov03@gmail.com>
2022-02-27 12:13:12 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var elBody = document.querySelector("body");
+var elBtn = document.querySelector("#header__button");
+
+elBtn.addEventListener("click" , function(){
+  elBody.classList.add("dark-mody")
+})
